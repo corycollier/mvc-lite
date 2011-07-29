7b68f0b01e9ebfcea38cadc1d214f4de0c9445d9
Cory Collier <corycollier@corycollier.com>
2011-07-28 22:41:57 -0400

    adding the missing filter unit tests

---
+++ tests/FilterTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Unit tests for the Lib_View class
+ * 
+ * @category    MVCLite
+ * @package     Tests
+ * @subpackage  View
+ * @since       File available since release 1.0.6
+ * @author      Cory Collier <corycollier@corycollier.com>
+ */
+/**
+ * Unit tests for the Lib_View class
+ * 
+ * @category    MVCLite
+ * @package     Tests
+ * @subpackage  View
+ * @since       Class available since release 1.0.6
+ * @author      Cory Collier <corycollier@corycollier.com>
+ */
+
+class ViewTest
+extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * The setup method, called before each test
+     */
+    public function setUp ( )
+    {
+
+    } // END function setup
+
+    /**
+     * The tear down hook, called after each test
+     */
+    public function tearDown ( )
+    {
+
+    } // END function tearDown
+
+    /**
+     * tests the filter's ability to change dashes to camel case
+     */
+    public function test_dashToCamelCase ( )
+    {
+        $this->markTestIncomplete('not ready yet');
+
+    } // END function test_dashToCamelCase
+
+    public function test_camelCaseToDash ( )
+    {
+        $unfiltered = 'ThisIsSomethingElse';
+
+        $expected = 'This-Is-Something-Else';
+
+        $result = Lib_Filter::camelCaseToDash($unfiltered);
+
+        $this->assertSame($expected, $result);
+
+    } // END function test_camelCaseToDash
+
+    public function test_underscoreToDirectorySeparator ( )
+    {
+        $this->markTestIncomplete('not ready yet');
+
+    } // END function test_underscoreToDirectorySeparator
+
+    public function test_underscoreToCamelCase ( )
+    {
+        $this->markTestIncomplete('not ready yet');
+
+    } // END function test_underscoreToCamelCase
+} // END class ModelTest
