3d7ed119a8ff741ec2a8c1b2b1bc641014537974
Cory Collier <corycollier@corycollier.com>
2015-12-15 09:41:13 -0500

    Adding unit tests for traits

---
+++ tests/Traits/FilterChainTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Class to test the MvcLite\Traits\FilterChain trait
+ *
+ * @category    PHP
+ * @package     MvcLite
+ * @subpackage  Tests
+ * @since       File available since release 3.1.x
+ * @author      Cory Collier <corycollier@corycollier.com>
+ */
+
+namespace MvcLite;
+
+use MvcLite\Traits\FilterChain as FilterChainTrait;
+
+/**
+ * Class to test the MvcLite\Traits\FilterChain trait
+ *
+ * @category    PHP
+ * @package     MvcLite
+ * @subpackage  Tests
+ * @since       File available since release 3.1.0
+ * @author      Cory Collier <corycollier@corycollier.com>
+ */
+class FilterChainTraitsTest extends TestCase
+{
+    /**
+     * Tests the getFilterChain method of the trait
+     */
+    public function testGetFilterChain()
+    {
+        $sut = new TestFixtureFilterChainTrait;
+        $result = $sut->getFilterChain(['StringToLower', 'StringToUpper']);
+        $this->assertInstanceOf('\MvcLite\FilterChain', $result);
+    }
+}
+
+class TestFixtureFilterChainTrait
+{
+    use FilterChainTrait;
+}
---
+++ tests/Traits/SessionTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Class to test the MvcLite\Traits\Session trait
+ *
+ * @category    PHP
+ * @package     MvcLite
+ * @subpackage  Tests
+ * @since       File available since release 3.1.x
+ * @author      Cory Collier <corycollier@corycollier.com>
+ */
+
+namespace MvcLite;
+
+use MvcLite\Traits\Session as SessionTrait;
+
+/**
+ * Class to test the MvcLite\Traits\Session trait
+ *
+ * @category    PHP
+ * @package     MvcLite
+ * @subpackage  Tests
+ * @since       File available since release 3.1.0
+ * @author      Cory Collier <corycollier@corycollier.com>
+ */
+class SessionTraitsTest extends TestCase
+{
+    /**
+     * Tests the getSession method of the trait
+     */
+    public function testGetSession()
+    {
+        $sut = new TestFixtureSessionTrait;
+        $result = $sut->getSession();
+        $this->assertInstanceOf('\MvcLite\Session', $result);
+    }
+}
+
+class TestFixtureSessionTrait
+{
+    use SessionTrait;
+}
